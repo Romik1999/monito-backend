0cf2b39b4a0c6f32b4af27647276735259e8f421
roman <usankin.roma@gmail.com>
2023-09-19 02:11:07 +0400

    create project, add products module

---
+++ src/moks/index.ts
@@ -0,0 +1,272 @@
+export const products = [
+    {
+        "_id": "6508c7353e1e93068fae491b",
+        "index": 0,
+        "guid": "844c1214-b258-4335-a9ab-26dfb126e560",
+        "isActive": false,
+        "balance": "$3,457.89",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "green",
+        "name": "Roslyn Patton",
+        "gender": "female",
+        "company": "QUARMONY",
+        "email": "roslynpatton@quarmony.com",
+        "phone": "+1 (815) 492-3577",
+        "address": "264 Trucklemans Lane, Thynedale, Florida, 3289",
+        "about": "Nostrud labore do laboris nostrud ipsum ullamco tempor adipisicing exercitation esse veniam. Consequat non magna deserunt dolor ad adipisicing ea reprehenderit occaecat officia pariatur ullamco. Aute esse nisi aliquip laborum commodo do. Incididunt aliquip Lorem incididunt tempor consequat. Reprehenderit excepteur aute ex qui dolor Lorem duis cupidatat.\r\n",
+        "registered": "2018-04-10T09:48:43 -04:00",
+        "latitude": -24.51983,
+        "longitude": 22.519172,
+        "tags": [
+            "eiusmod",
+            "magna",
+            "mollit",
+            "dolor",
+            "ea",
+            "fugiat",
+            "ad"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Connie Chaney"
+            },
+            {
+                "id": 1,
+                "name": "Kirsten Callahan"
+            },
+            {
+                "id": 2,
+                "name": "Carissa Zimmerman"
+            }
+        ],
+        "greeting": "Hello, Roslyn Patton! You have 2 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "6508c735d544aa2ca6a42dab",
+        "index": 1,
+        "guid": "7f03c72f-8c6c-429e-96e4-1f6cbe6361d2",
+        "isActive": false,
+        "balance": "$3,138.19",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "green",
+        "name": "Cole Alston",
+        "gender": "male",
+        "company": "ZILLA",
+        "email": "colealston@zilla.com",
+        "phone": "+1 (867) 564-3528",
+        "address": "858 Bogart Street, Loretto, Oklahoma, 8538",
+        "about": "Lorem consectetur ex eu ipsum officia dolor commodo commodo. Culpa exercitation id ut nulla minim commodo. Duis dolore excepteur officia eu excepteur eiusmod id ea incididunt do irure ad. Pariatur qui qui qui duis in proident eiusmod nisi. Enim incididunt laborum nulla incididunt id ex amet commodo. Sit nisi ea quis ea aute proident fugiat id quis aliquip. Voluptate officia nulla qui id sunt cillum sit incididunt esse.\r\n",
+        "registered": "2016-12-26T08:18:36 -04:00",
+        "latitude": -31.860819,
+        "longitude": -41.421355,
+        "tags": [
+            "laborum",
+            "consequat",
+            "incididunt",
+            "esse",
+            "deserunt",
+            "non",
+            "laborum"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Cotton Farley"
+            },
+            {
+                "id": 1,
+                "name": "Rosalyn Buchanan"
+            },
+            {
+                "id": 2,
+                "name": "Natalia Crane"
+            }
+        ],
+        "greeting": "Hello, Cole Alston! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "6508c735d8c210e0899a168a",
+        "index": 2,
+        "guid": "9bf8e2e4-2446-490c-81b6-94f773cda582",
+        "isActive": false,
+        "balance": "$2,425.06",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "blue",
+        "name": "Cabrera Glover",
+        "gender": "male",
+        "company": "PROWASTE",
+        "email": "cabreraglover@prowaste.com",
+        "phone": "+1 (822) 463-3795",
+        "address": "783 Rutledge Street, Sunriver, New York, 6792",
+        "about": "Consequat velit ullamco aliquip nostrud sit reprehenderit nostrud incididunt eiusmod officia veniam aliqua incididunt sit. Et irure voluptate dolor sint deserunt ex pariatur. Consequat pariatur aliquip aliqua dolor sit officia est ea ut commodo et.\r\n",
+        "registered": "2018-02-07T02:39:44 -04:00",
+        "latitude": -64.009328,
+        "longitude": -38.734267,
+        "tags": [
+            "Lorem",
+            "consectetur",
+            "ad",
+            "duis",
+            "do",
+            "anim",
+            "id"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Patton Maldonado"
+            },
+            {
+                "id": 1,
+                "name": "Gail Singleton"
+            },
+            {
+                "id": 2,
+                "name": "Kelsey David"
+            }
+        ],
+        "greeting": "Hello, Cabrera Glover! You have 5 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "6508c73586341930ca7c47ef",
+        "index": 3,
+        "guid": "d116406b-c12d-4911-8acf-422327b2960f",
+        "isActive": true,
+        "balance": "$3,579.44",
+        "picture": "http://placehold.it/32x32",
+        "age": 30,
+        "eyeColor": "green",
+        "name": "Brenda Fowler",
+        "gender": "female",
+        "company": "TALAE",
+        "email": "brendafowler@talae.com",
+        "phone": "+1 (927) 444-3735",
+        "address": "642 Campus Place, Bordelonville, California, 2201",
+        "about": "Pariatur sit in magna occaecat. Ex aute adipisicing esse excepteur eu sunt. Dolore voluptate consectetur irure mollit aute eiusmod ullamco eiusmod anim quis tempor. Tempor deserunt enim laboris et do veniam amet laboris elit minim ut irure. Occaecat eiusmod irure ipsum sunt est sint commodo proident. Cupidatat est officia id id commodo dolor est dolore. Velit ullamco eu deserunt esse enim Lorem ut dolore occaecat eu ea.\r\n",
+        "registered": "2021-03-17T02:29:49 -04:00",
+        "latitude": -70.843602,
+        "longitude": -20.539377,
+        "tags": [
+            "tempor",
+            "sunt",
+            "reprehenderit",
+            "ad",
+            "tempor",
+            "excepteur",
+            "sunt"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Petty Malone"
+            },
+            {
+                "id": 1,
+                "name": "Amy Daniels"
+            },
+            {
+                "id": 2,
+                "name": "Cassandra Tran"
+            }
+        ],
+        "greeting": "Hello, Brenda Fowler! You have 2 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "6508c7350bf354a23bbe4b6a",
+        "index": 4,
+        "guid": "17c073b2-2c46-45d2-9d40-539db1ae75fe",
+        "isActive": false,
+        "balance": "$3,826.73",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "blue",
+        "name": "Green Strickland",
+        "gender": "male",
+        "company": "STELAECOR",
+        "email": "greenstrickland@stelaecor.com",
+        "phone": "+1 (868) 518-3417",
+        "address": "708 Nautilus Avenue, Dunlo, New Jersey, 6155",
+        "about": "Enim voluptate nostrud reprehenderit fugiat laborum commodo incididunt anim veniam ullamco est nulla aliquip. Quis sint nisi nostrud sit adipisicing laboris. Incididunt amet nisi incididunt proident sit et nostrud magna.\r\n",
+        "registered": "2021-03-05T07:41:49 -04:00",
+        "latitude": -53.548884,
+        "longitude": -144.733305,
+        "tags": [
+            "cillum",
+            "amet",
+            "nostrud",
+            "fugiat",
+            "id",
+            "cillum",
+            "incididunt"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Park Buck"
+            },
+            {
+                "id": 1,
+                "name": "Goldie Osborn"
+            },
+            {
+                "id": 2,
+                "name": "Cameron Norman"
+            }
+        ],
+        "greeting": "Hello, Green Strickland! You have 1 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "6508c73532d56c8f98abe12f",
+        "index": 5,
+        "guid": "b2f4c9dc-fbce-4a23-82df-e2fb7afd461c",
+        "isActive": false,
+        "balance": "$2,617.42",
+        "picture": "http://placehold.it/32x32",
+        "age": 27,
+        "eyeColor": "green",
+        "name": "Young Harding",
+        "gender": "male",
+        "company": "TELEQUIET",
+        "email": "youngharding@telequiet.com",
+        "phone": "+1 (841) 497-3011",
+        "address": "948 Lynch Street, Suitland, Minnesota, 2595",
+        "about": "Id ad magna irure aliqua proident aliquip nostrud non reprehenderit cupidatat excepteur nostrud reprehenderit. Veniam anim deserunt elit minim fugiat adipisicing fugiat magna consequat Lorem ullamco ullamco excepteur. Ea anim voluptate occaecat ipsum cupidatat excepteur. Pariatur aliqua do id eiusmod velit.\r\n",
+        "registered": "2014-10-06T10:03:46 -04:00",
+        "latitude": 51.90715,
+        "longitude": -57.271779,
+        "tags": [
+            "pariatur",
+            "do",
+            "sunt",
+            "qui",
+            "sunt",
+            "excepteur",
+            "ad"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Hopper Holloway"
+            },
+            {
+                "id": 1,
+                "name": "Butler Schroeder"
+            },
+            {
+                "id": 2,
+                "name": "Leola Santana"
+            }
+        ],
+        "greeting": "Hello, Young Harding! You have 1 unread messages.",
+        "favoriteFruit": "banana"
+    }
+]
